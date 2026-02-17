b8efdbdd58a8cc60cd01c0268cc67e4864e992eb
Vijay Hardaha <vijayhardaha@gmail.com>
2026-02-17 16:57:39 +0530

    refactor: streamline login and enhance functions documentation and comments

--- extension.js
+++ extension.js
@@ -1,8 +1,9 @@
-const veganIpsum = require("vegan-ipsum").veganIpsum;
 const vscode = require("vscode");
+const veganIpsum = require("vegan-ipsum").veganIpsum;
 
 /**
  * Activates the extension and registers commands.
+ *
  * @param {vscode.ExtensionContext} context - The extension context.
  */
 function activate(context) {
@@ -19,18 +20,28 @@ function activate(context) {
 
 /**
  * Inserts generated text into the active editor.
+ *
  * @param {Object} lorem - The options for text generation.
  * @param {number} lorem.count - The number of units to generate.
  * @param {string} lorem.units - The type of units to generate (e.g., "sentences", "paragraphs").
  */
 function insertText(lorem) {
+  // Get the active text editor
   var editor = vscode.window.activeTextEditor;
-  editor.edit((edit) =>
-    editor.selections.forEach((selection) => {
-      edit.delete(selection);
-      edit.insert(selection.start, veganIpsum(lorem));
-    })
-  );
+
+  // If an editor is active, generate the vegan ipsum text and insert it into the editor
+  if (editor) {
+    // Generate the vegan ipsum text and insert it into the editor
+    editor.edit((edit) =>
+      // Loop through each selection in the editor and replace it with the generated vegan ipsum text
+      editor.selections.forEach((selection) => {
+        // Delete the current selection and insert the generated vegan ipsum text at the start of the selection
+        edit.delete(selection);
+        // Insert the generated vegan ipsum text at the start of the selection
+        edit.insert(selection.start, veganIpsum(lorem));
+      })
+    );
+  }
 }
 
 /**
@@ -58,13 +69,18 @@ function generateParagraph() {
  */
 async function generateMultipleParagraphs() {
   const items = [];
+
+  // Populate the items array with numbers from 2 to 10 as strings
   for (let i = 2; i <= 10; i++) {
     items.push(i.toString());
   }
 
+  // Show a quick pick dialog to the user with the items array and a placeholder text
   const count = await vscode.window.showQuickPick(items, {
     placeHolder: "How many paragraphs?",
   });
+
+  // If the user cancels the quick pick dialog, count will be undefined, so we check for that before proceeding
   if (!count) {
     return;
   }
